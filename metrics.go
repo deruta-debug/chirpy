e57fa4bed20721828e6c6f62aba24d278b6bdc3e
deruta <deruta.fuzzy377@gmail.com>
2025-12-21 18:15:17 -0500

    add admin namespace

--- metrics.go
+++ metrics.go
@@ -6,9 +6,17 @@ import (
 )
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
-	w.Write(fmt.Appendf([]byte{}, "Hits: %v", cfg.fileserverHits.Load()))
+	body := fmt.Sprintf(`
+<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>
+		`, cfg.fileserverHits.Load())
+	w.Write([]byte(body))
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
