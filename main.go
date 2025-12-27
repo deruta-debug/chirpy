4e68baa8f6b44e81d60731a7756f4cf5ac954a9c
deruta <deruta.fuzzy377@gmail.com>
2025-12-26 21:53:03 -0500

    add POST `/api/chirps` endpoint for creating chirp

--- main.go
+++ main.go
@@ -50,9 +50,10 @@ func main() {
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fsHandler))
 
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
-	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
 
 	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
+
+	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
 
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
